d2c9e5b896e1e9ac82c0d172e88614aa1ac75b63
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-04-22 12:24:02 -0300

    format code with autopep8

--- university/students/models.py
+++ university/students/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django_countries.fields import CountryField
 
-# Create your models here.
-
 """
 A class representing students.
 
@@ -21,6 +19,8 @@ Attributes:
 Methods:
     __str__(): Returns a string representation of the student.
 """
+
+
 class Students(models.Model):
     class GenderChoices(models.TextChoices):
         MALE = "M", "Male"
@@ -28,22 +28,36 @@ class Students(models.Model):
         OTHER = "X", "Other"
 
     first_name = models.CharField(
-        max_length=50, verbose_name='First Name', help_text='Enter your first name')
-    middle_name = models.CharField(max_length=50, null=True, blank=True,
-                                   verbose_name='Middle Name', help_text='Enter your middle name')
+        max_length=50, verbose_name="First Name", help_text="Enter your first name"
+    )
+    middle_name = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        verbose_name="Middle Name",
+        help_text="Enter your middle name",
+    )
     last_name = models.CharField(
-        max_length=50, verbose_name='Last Name', help_text='Enter your last name')
+        max_length=50, verbose_name="Last Name", help_text="Enter your last name"
+    )
     address = models.TextField(
-        max_length=254, verbose_name='Address', help_text='Enter your address')
+        max_length=254, verbose_name="Address", help_text="Enter your address"
+    )
     birth_country = CountryField(
-        null=False, blank=False, help_text='Select your birth country')
+        null=False, blank=False, help_text="Select your birth country"
+    )
     email = models.EmailField(
-        null=False, verbose_name='Email Address', help_text='Enter your email')
-    date_of_birth = models.DateField(help_text='Enter your birthdate')
-    gender = models.CharField(max_length=1, null=False, blank=False,
-                              choices=GenderChoices.choices, help_text='Select your gender')
-    cellphone = PhoneNumberField(
-        unique=True, help_text='Enter your phone number')
+        null=False, verbose_name="Email Address", help_text="Enter your email"
+    )
+    date_of_birth = models.DateField(help_text="Enter your birthdate")
+    gender = models.CharField(
+        max_length=1,
+        null=False,
+        blank=False,
+        choices=GenderChoices.choices,
+        help_text="Select your gender",
+    )
+    cellphone = PhoneNumberField(unique=True, help_text="Enter your phone number")
 
     def __str__(self) -> str:
         return "({0}) {1}, {2}".format(self.id, self.last_name, self.first_name)
