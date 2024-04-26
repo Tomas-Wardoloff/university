74d4ea6d1a06b4f6aa74d2761a80368337e454d9
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-04-25 23:41:15 -0300

    create views from drf for degrees

--- university/degree/views.py
+++ university/degree/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import DegreeSerializer
+from .models import Degrees
 
-# Create your views here.
+
+class DegreeViewSet(viewsets.ModelViewSet):
+    queryset = Degrees.objects.all()
+    serializer_class = DegreeSerializer
