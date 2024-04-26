6abe319ed6fef3ee0c1af0f3cbc11cc95c066be8
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-04-26 00:10:05 -0300

    create views from drf for students

--- university/students/views.py
+++ university/students/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import StudentSerializer
+from .models import Students
 
-# Create your views here.
+
+class StudentsViewSet(viewsets.ModelViewSet):
+    queryset = Students.objects.all()
+    serializer_class = StudentSerializer
