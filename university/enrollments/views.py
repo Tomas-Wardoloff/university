f49ec512e1ff247e84fb94df54977bd6f8b80512
tomas-wardoloff <tomaswardoloff@gmail.com>
2024-04-26 00:10:50 -0300

    create views from drf for enrollments

--- university/enrollments/views.py
+++ university/enrollments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CourseEnrollmentSerializer, DegreeEnrollmentSerializer
+from .models import CourseEnrollment, DegreeEnrollment
 
-# Create your views here.
+
+class CourseEnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = CourseEnrollment.objects.all()
+    serializer_class = CourseEnrollmentSerializer
+
+
+class DegreeEnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = DegreeEnrollment.objects.all()
+    serializer_class = DegreeEnrollmentSerializer
